9522ae22ebc49e42b849c93c6b42d8446cc76f5b
quincysmiith <8ca53525@opayq.com>
2020-10-18 10:41:13 +0000

    first commit

---
+++ myproject.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+@app.route("/cheese")
+def cheese():
+    return "<h1> I love Cheese!!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Marquin subdomain flask app
+
+A flask application for use on marquinsmith.com
+
+To do:
+
+- Food logger
+- Workout logger
+- saved html viewer
+- har file analyser for woolworths adobe calls
